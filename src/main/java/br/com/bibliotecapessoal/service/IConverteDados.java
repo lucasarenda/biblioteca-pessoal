66d9652a70860bf924ba49d4e1429c3cdb9da43b
lucas <lucas.arenda@gmail.com>
2026-08-11 17:15:05 -0300

    feat: refatora consumo da API e adiciona atributos em Livro

--- src/main/java/br/com/bibliotecapessoal/service/IConverteDados.java
+++
@@ -1,9 +0,0 @@
-package br.com.bibliotecapessoal.service;
-
-import java.util.List;
-
-public interface IConverteDados {
-    <T> T obterDados(String json, Class <T> classe);
-
-    <T> List<T> obterLista(String json, Class <T> classe);
-}
